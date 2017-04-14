a248ea7e32b5931c39482d7b4f0da2a7e8175be5
Weston Schmidt <Weston_Schmidt@cable.comcast.com>
2017-04-14 13:58:58 -0700

    Fix code formatting.

--- src/main.c
+++ src/main.c
@@ -44,8 +44,8 @@
 /*----------------------------------------------------------------------------*/
 int main( int argc, char *argv[] )
 {
-    (void) argc;
-    (void) argv;
+    ( void ) argc;
+    ( void ) argv;
 
     return 0;
 }
